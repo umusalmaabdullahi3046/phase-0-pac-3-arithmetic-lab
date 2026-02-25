5c316917acbb2eba62689dacb8f7d6aa7711441a
umusalma <umusalmaabdullahi3046@gmail.com>
2026-02-25 15:13:45 +0300

    done

---
+++ index.js
@@ -0,0 +1,55 @@
+add(4, 5);
+function add(a, b) {
+    console.log(a + b);
+}
+
+subtract(7, 5)
+function subtract(a, b) {
+    console.log(a - b);
+}
+
+multiply(6, 5);
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+divide(10, 2);
+function divide(a, b) {
+    console.log(a / b );
+}
+
+add(1000, 38);
+function add(c, d) {
+    return c + d;
+}
+
+subtract(20, 10);
+function subtract(c, d) {
+    return c - d;
+}
+
+multiply(10, 8);
+function multiply(c, d) {
+    return c * d;
+}
+
+divide(100, 2);
+function divide(c, d) {
+    return c / d;
+}
+
+function increment(n) {
+  return n + 1;
+}
+
+function decrement(n) { 
+  return n - 1;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
